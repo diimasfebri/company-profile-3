332987b53f41b47cdcc84812654bf2bcd4124244
diimasfebri <dimas.febri21@gmail.com>
2021-08-29 11:00:44 +0700

    dim

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,59 +1,54 @@
 <template>
 <div class="container">
- <div class="top">
+  <div class="top">
     <div class="left">
-<<<<<<< HEAD
-=======
-      <div class="dragdown" @click="opacity=1">
-        <div class="list">ALL</div>
+      <div class="dragdown">
         <div class="list">CUSTOM DESIGN</div>
         <div class="list">SCREEN PRINTING</div>
         <div class="list">HEADWEAR</div>
         <div class="list">CUT & SAW</div>
         <div class="list">ACCESSORIES</div>
       </div>
->>>>>>> 7415f256b78d8fdb63a22b67060cbd2e985a5345
-      <span class="button">SERVICES</span>
-      <span class="button">OUR WORK</span>
-      <span class="button">OUR PROCESS</span>
-      <span class="button">ABOUT US</span>
+        <span class="button">SERVICES</span>
+        <span class="button">OUR WORK</span>
+        <span class="button">OUR PROGRESS</span>
+        <span class="button">ABOUT US</span>
     </div>
     <div class="center">
-      <div class="image">
-        <img src="https://cdn.shopify.com/s/files/1/0274/1389/files/suavecito-logo-full.png?height=628&pad_color=fff&v=1568385277&width=1200" alt="">
-      </div>
+        <div class="image">
+          <img src="/suavecito.png" alt="">
+        </div>
     </div>
     <div class="right">
-      <div class="button">
-        <span class="button-hire"> HIRE US</span>
-      </div>
+        <div class="button">
+          <span class="button-hire"> HIRE US</span>
+        </div>
     </div>
- </div>
- <nuxt />
- <div class="bottom">
+  </div>
+   <nuxt />
+  <div class="bottom">
    <div class="top-section">
      <div class="left">
        <span class="address">Jl. Ketintang Baru IV No.17, Surabaya, Jawa Timur</span>
        <span class="number">+62 857-3378-4892</span>
        <div class="email-container">
          <span class="email">Email : </span>
-         <span class="name">redian@gmail.com</span>
+         <span class="name"> redian@gmail.com</span>
        </div>
 
      </div>
      <div class="right">
+       <span class="desc">SIGN UP FOR OUR NEWSLETTER</span>
        <div class="sign-up">
-         <span>SIGN UP FOR OUR NEWSLETTER</span>
        </div>
        <div class="submit">
-         <!-- <input type="email"> -->
        </div>
      </div>
-   </div>
+    </div>
    <div class="center-section">
-     <span> TEST</span>
-   </div>
-   <div class="bottom-section">
+     <!-- <span> TEST</span> -->
+    </div>
+   <!-- <div class="bottom-section">
      <div class="left">
       <span class="button">ABOUT US</span>
       <span class="button">OUR WORK</span>
@@ -66,10 +61,11 @@
       <span class="button">CUT AND SEW</span>
       <span class="button">ACCESSORIES</span>
      </div>
+    </div> -->
+   <div class="social-media">
+    </div>
    </div>
-   <div class="social-media"></div>
  </div>
-</div> 
 </template>
 
 
@@ -89,19 +85,18 @@
     width: 100%;
     height: 3rem;
     display: flex;
-    color:red; 
+    align-items: center;
     margin-top: 2rem;
     z-index:3000;
     .left{
-      position: relative;
+      position: absolute;
       display: flex;
       flex-direction: row;
       width: 100%;
+      left: 0;
       justify-content: flex-start;
       align-items: center;
       margin-left: 2.5rem;
-<<<<<<< HEAD
-=======
       .dragdown{
           position: absolute;
           width: 6rem;
@@ -115,18 +110,16 @@
           align-items: flex-start;
           line-height: 1.75;
           padding-left: 1rem;
-          opacity: 0;
           .list{
             font-family: 'Antonio';
             font-weight: 700;
             font-size: 15px;
           }
       }
->>>>>>> 7415f256b78d8fdb63a22b67060cbd2e985a5345
       span.button{
         font-family: 'Antonio';
         font-weight: 700;
-        color: black;
+        color: white;
         font-size: 18px;
         margin: 14px;
       }
@@ -135,22 +128,22 @@
       position: relative;
       width: 100%;
       display: flex;
-      right: 9rem;
       justify-content: center;
       align-items: center;
       .image{
         position: absolute;
         img{
-          width: 5.5rem;
-          height: 2.5rem;
+          width: 6.5rem;
+          height: 5.5rem;
         }
       }
     }
     .right{
-      position: relative;
+      position: absolute;
       display: flex;
       width: 6rem;
       height: 100%;
+      right: 0;
       flex-direction: column;
       justify-content: center;
       align-items: center;
@@ -189,27 +182,71 @@
     flex-direction: column;
     .top-section{ 
       position: relative;
-      top: 2rem;
       width: 40%;
       display: flex;
+      top: 5rem;
       flex-direction: row;
       justify-content: flex-start;
       align-items: center;
       .left{
         position: relative;
         display: flex;
-        width: 100%;
+        width: 40%;
+        flex-direction: column;
         justify-content: flex-start;
         align-items: flex-start;
-        span.address{
-        font-family: "Quicksand";
-        font-size: 1rem;
-
+          span.address{
+          font-family: "Quicksand";
+          font-size: 18px;
+          color: #f0EEE3;
+          }
+          span.number{
+          font-family: "Quicksand";
+          font-size: 18px;
+          color: #B48645;
+          }
+          .email-container{
+            position: relative;
+            display: flex;
+            justify-content: flex-start;
+            padding-top: 1rem;
+            align-items: flex-start;
+            font-family: 'Quicksand';
+            font-size: 18px;
+            span.email{
+            color: #f0EEE3;
+            }
+            span.name {
+            position: relative;
+            display: flex;
+            left: 0.4rem;
+            color: #b48645;
+            }
+          }
+      }
+      .right{
+        position: relative;
+        display: flex;
+        width: 60%;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        span.desc{
+          position: relative;
+          font-family: "Antonio";
+          font-size: 18px;
+          font-weight: 400;
+          color: #f0EEE3;
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
         }
       }
     }
     .center-section{
+      span{
 
+      }
     }
     .bottom-section{
       .left{
@@ -219,8 +256,9 @@
         justify-content: flex-start;
         align-items: flex-start;
         span.button{
-           font-family: "Quicksand";
-           font-size: 1rem;
+          font-family: "Quicksand";
+          font-size: 1.5rem;
+          color: white;
         }
       }
       .right{
@@ -234,6 +272,9 @@
           font-size: 1rem;
         }
       }
+    }
+    .social-media{
+
     }
   }
 }
