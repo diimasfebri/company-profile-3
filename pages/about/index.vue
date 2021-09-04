80def10b4d6dd7fe3bda2a16b72ff4ae6bbb5070
mirdaazzah18 <azzahmirda@gmail.com>
2021-09-04 15:05:49 +0700

    ca

---
+++ pages/about/index.vue
@@ -0,0 +1,381 @@
+<template>
+<div class="body-container"> 
+  <div class="slide-one">
+    <img class="bg-content" src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80">
+  <div class="desc-container">
+    <span class="desc-1"> THE STORY </span>
+    </div>  
+  </div>
+  <div class="slide-two">
+    <img class="bg-content" src="https://images.unsplash.com/photo-1550684376-efcbd6e3f031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" >
+  <div class="button-container">
+      <span class="desc">ABOUT US</span>
+      <span class="desc-1">OUR STUDIO & SERVICES</span>
+      <span class="desc-2">We are a full package design agency and production house based in Seal Beach, California. Whether you're looking
+          to grow your business or expand your swag, we're here to help. We offer best-in-class screen printing, custom design services, and 
+          a variety of wereables designed to showcase your brand.</span>
+    </div>
+    <div class="content-container">
+        <img src="" alt="">
+  </div>
+  </div>
+  <div class="slide-three">
+    <div class="img-container">
+        <img class="bg-content" src="https://images.prismic.io/andersonbrothers/a2e85e69-8694-414e-8778-ad7d68f6818f_bos-big.jpeg?auto=compress,format&w=900&h=900&fit=crop&q=85&f=center">
+     </div>
+  <div class="desc-container">
+    <span class="desc-1">OUR HISTORY</span>
+    <span class="desc-2">Our goal is to help grow your brand through custom design and premium apparel. We offer quick turnaround times and affordable pricing. We are a full package design agency and production house started by John and Mike Anderson, based in Seal Beach, California.
+    ABDS can promise a substantial growth in sales by offering the best quality designs, products and years of experience.John & Mike also created the iconic men’s brand Tankfarm & Co. in 2003. Tankfarm has brightened the doors of America’s best retail stores such as Planet Blue, Nordstrom & Bloomingdales.
+    That retail experience has given the brothers the tools to build your brand a world class merchandise program. In 2018, the Andersons teamed up with Marcus Lemonis after being featured on the popular business show "The Profit" on CNBC.
+    If you’re ever in town, come by for a cold beer or a hot cup of joe.</span>
+  </div>
+  </div>
+  <div class="slide-four">
+    <div class="desc-container">
+      <span class="desc-1">THE TEAM</span>
+      <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/89d584b8-3691-4255-80d4-b8ebcd5b1350_John-Anderson-President.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> John </span>
+          <span class="role"> President </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/83eae39b-48fb-4e72-a27e-edf48fbd5425_Mike-Anderson-Creative-Director.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Mike </span>
+          <span class="role"> Creative Director </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/29830ffb-eb83-48d0-9830-d53bf63574d5_Aaron-Ashton-Sales-%26-Marketing.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Aaron </span>
+          <span class="role"> Marketing </span>
+        </div>
+      </div>  
+      <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/6eb97008-65a2-427b-ab41-a835617f3abb_AfterlightImage+22.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Esmeralda </span>
+          <span class="role"> Operations Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/bd632467-16f3-429a-abdc-a342cf41c708_Debbi-Wong-Accounting-Manager.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Debbi </span>
+          <span class="role"> Accounting Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/2117d343-7156-4e6f-9002-090ea6342e42_Kenny-Novak-Graphic-Design.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Kenny </span>
+          <span class="role"> Graphic Designer </span>
+        </div>
+      </div>  
+       <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/4ecfbc98-ced8-40ae-8082-ea5c65e06ba8_Madison-Bailey-Production-Manager.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Madison </span>
+          <span class="role"> Production Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/e600c588-2fed-4de6-933a-68406fc2d409_Tempest-Orman-Accounting-Asst.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Tempestt </span>
+          <span class="role"> Accounting Assistent </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/d4e66e1a-98af-4df8-bea9-3f1ace0481c0_Jen-Valdivia-Produciton-Asst-.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Jen </span>
+          <span class="role"> Production Assistant </span>
+        </div>
+      </div> 
+      <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/bffdf4de-2a14-43b1-b47b-4ad306efc9d2_Alex-Coleman-Logistics-Manager-.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Alex </span>
+          <span class="role"> Logistics Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/000de66d-2ba2-4842-9383-305d3ddc72e8_AfterlightImage+20.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Jeff </span>
+          <span class="role"> Graphic Designer </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/55f2fb68-1c1d-45c5-a0a4-3a877915636c_AfterlightImage+21.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Duke </span>
+          <span class="role"> Retail Merchandiser </span>
+        </div>
+      </div>
+       <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/1eb15567-0d64-4d42-a3cb-8bd6d08fdf1f_AfterlightImage+23.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Ashley </span>
+          <span class="role"> Retail Merchaniser </span>
+        </div>
+        <div class="card button">
+          <span class="name-1"> WE'RE GROWING </span>
+          <span class="role-1"> JOIN THE FAMILY</span>
+        </div>
+       
+      </div>    
+    </div>
+  </div>
+    <div class="slide-five">
+        <span class="desc"></span>
+   </div> 
+</div>
+</template>
+
+<style lang="scss" scoped>
+.body-container{
+  position: relative;
+  width: 100vw;
+  min-height: 100vh;
+  min-height: calc((var(--vh, 1vh) * 100));
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+.slide-one{
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+   img.bg-content{
+      position: absolute;
+      width: 100vw;
+      height: 100vh;
+    }
+    .desc-container{
+        position: relative;
+        display: flex;
+        width: 40%;
+        height: 100%;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        margin-left:  4rem;
+        margin-top: 2rem;
+        line-height: 1.5;
+        span.desc{
+            font-family: 'Antonio';
+            color: #b48645;
+            font-size: 1rem;
+            font-weight: 900;
+        }
+        span.desc-1{
+          color: white;
+          font-family: 'Antonio';
+          font-size: 3rem;
+          font-weight: 900;
+        }
+      .button{
+        position: relative;
+        display: flex;
+        width: 11rem;
+        height: 1.5rem;
+        background: white;
+        justify-content: center;
+        align-items: center;
+        margin-top: 0.5rem;
+        span{
+          color: black;
+          font-family: Antonio;
+          font-size: 0.8rem;
+          font-weight: 900;
+        }
+      }
+    }
+ }
+ .slide-two{
+   position: relative;
+   width: 100vw;
+   height: 60vh;
+   display: flex;
+    img.bg-content{
+      position: absolute;
+      width: 100vw;
+      height: 60vh;
+ }
+ .button-container{
+    position: relative;
+    display: flex;
+    width: 55%;
+    height: 60vh;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    line-height: 1.5;
+    margin-left: 3rem;;
+    span.desc{
+        font-family: 'Antonio';
+        color: white;
+        font-size: 3rem;
+        font-weight: 900;
+        margin-bottom: 1rem;
+      }
+    span.desc-1{
+        font-family: 'Antonio';
+        font-size: 1rem;
+        color: white;
+        margin-bottom: 0.5rem;
+    }
+    span.desc-2{
+        font-family: 'Quicksand';
+        font-size: 0.7rem;
+        color: white;
+    }
+    .content-container{
+        position: relative;
+        display: flex;
+        width: 80%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+     img{
+       position: relative;
+       display: flex;
+       width: 12rem;
+       height: 8rem;
+     }
+    }
+ }
+ }
+.slide-three{
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  .img-container{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+      img.bg-content{
+         position: relative;
+         width: 50vw;
+         height: 100vh;
+         object-fit: cover;
+      }
+  }
+    .desc-container{
+      position: relative;
+      display: flex;
+      width: 40%;
+      height: 100%;
+      margin-left: 30rem;
+      padding: 5rem 2rem;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+    span.desc-1{
+        font-family: Antonio;
+        color: black;
+        font-size: 1rem;
+        font-weight: 900;
+        transform: translateY(-2.5rem);
+    }
+    span.desc-2{
+        font-family: Quicksand;
+        color: black;
+        font-size: 0.7rem;
+        transform: translateY(-1rem);
+    }
+   }
+  }
+.slide-four{
+  position: relative;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+.desc-container{
+  position: relative;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  span.desc-1{
+        font-family: Antonio;
+        color: black;
+        margin: 4rem 0 2rem 0;
+        font-size: 1rem;
+        font-weight: 900;
+    }
+  .card-list{
+    position: relative;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+      .card{
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        img{
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: flex-start;
+          margin-left: 1rem;
+        }
+        span.name{
+          font-family: 'Quicksand';
+          font-size: 18px;
+          margin-top: 0.5rem;
+          margin-bottom: 0.5rem;
+          font-weight: bold;
+          justify-content: flex-start;
+          align-items: flex-start;
+          display: flex;
+          margin-left: 1rem;
+        }
+        span.role{
+          font-family: 'Quicksand';
+          font-size: 15px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          display: flex;
+          margin-left: 1rem;
+          margin-bottom: 1rem;
+        }
+        &.button{
+          background: #f4f4f4;
+          width: 330px;
+          height: 370px;
+          transform: translateY(-1.89rem);
+          span.name-1{
+            font-family: 'Quicksand';
+            font-size: 18px;
+            margin-top: 0.5rem;
+            margin-bottom: 0.5rem;
+            font-weight: bold;
+            justify-content: flex-start;
+            align-items: flex-start;
+            display: flex;
+            margin-left: 1rem;
+          }
+          span.role-1{
+            font-family: 'Quicksand';
+            font-size: 1px;
+            margin-top: 0.5rem;
+            margin-bottom: 0.5rem;
+            font-weight: bold;
+            justify-content: flex-start;
+            align-items: flex-start;
+            display: flex;
+            margin-left: 1rem;
+          }
+          &:hover{
+            background: #b48645;
+          }
+        }
+      }
+  }
+}
+}
+}
+</style>
