9a605d612c3771f43daafd391b8c9a93860d34fe
hildina <hildina.mulyaaii@gmail.com>
2021-09-04 15:05:03 +0700

    hil

---
+++ pages/contact/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="slide-one">
+            <img class="bg-content" src="https://images.unsplash.com/uploads/1412273646059cc129bb5/eae0a1da?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80">
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.container{
+  position: relative;
+  width: 100vw;
+  min-height: 100vh;
+  min-height: calc((var(--vh, 1vh) * 100));
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  .slide-one{
+      
+  }
+}
+</style>
