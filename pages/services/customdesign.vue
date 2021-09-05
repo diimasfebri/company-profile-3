4f72b3dcb68198105e244e8db35c327198c95938
mirdaazzah18 <azzahmirda@gmail.com>
2021-09-05 15:16:01 +0700

    ca

---
+++ pages/services/customdesign.vue
@@ -0,0 +1,479 @@
+<template>
+<div class="body-container"> 
+  <div class="slide-one">
+    <img class="bg-content" src="https://images.unsplash.com/photo-1441984261150-55796ff52afc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=839&q=80">
+  <div class="desc-container">
+    <span class="desc-1"> CUSTOM DESIGN </span>
+    </div>  
+  </div>
+  <div class="slide-two">
+    <div class="img-container">
+        <img class="bg-content" src="https://images.prismic.io/andersonbrothers/635650b4-df28-4171-9d5d-6534d5d81a1a_73475344_101021104658431_7650227357011818624_n.jpeg?auto=compress,format&w=750&h=676&fit=crop&q=85&f=center">
+     </div>
+    <div class="desc-container">
+        <span class="desc">WHEN YOU WORK WITH US, YOU WORK WITH THE BEST</span>
+        <span class="desc-1">The folks we help aren’t just customers to us – these are brands we believe in, brands we wear and support. That’s why we’ve built a world-class design team to help bring your vision to life. Our designers and in-house illustrators dive deep into your logo design, color palette and messaging in order to get to the heart of your brand.  When it comes to the design work, we keep it simple. After our design team dives into your brand, you’ll work collaboratively with us to create your collection. We’ll talk about your story, maybe share a laugh or two, and really get to know who you, and your brand, are.</span>
+      </div>  
+    <div class="img-list">
+      <img src="https://images.prismic.io/andersonbrothers/360ec377-867c-4e9a-b71c-c6d6beefbb35_107941959_145001300536455_2962723900008999164_n.jpeg?auto=compress,format&w=968&h=544&fit=crop&q=85&f=center">
+      <img src="https://images.prismic.io/andersonbrothers/332e50f3-2b70-4a3e-8353-ccc28c04cdd4_118614890_167168778236020_7885514944768749926_n.jpeg?auto=compress,format&w=397&h=544&fit=crop&q=85&f=center">
+    </div>  
+  </div>
+  <div class="slide-three">
+    <div class="img-container">
+        <img class="bg-content" src="">
+     </div>
+  <div class="desc-container">
+    <span class="desc-1"></span>
+    <span class="desc-2"></span>
+  </div>
+  </div>
+  <div class="slide-four">
+    <div class="desc-container">
+      <span class="desc-1">THE TEAM</span>
+      <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/89d584b8-3691-4255-80d4-b8ebcd5b1350_John-Anderson-President.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> John </span>
+          <span class="role"> President </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/83eae39b-48fb-4e72-a27e-edf48fbd5425_Mike-Anderson-Creative-Director.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Mike </span>
+          <span class="role"> Creative Director </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/29830ffb-eb83-48d0-9830-d53bf63574d5_Aaron-Ashton-Sales-%26-Marketing.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Aaron </span>
+          <span class="role"> Marketing </span>
+        </div>
+      </div>  
+      <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/6eb97008-65a2-427b-ab41-a835617f3abb_AfterlightImage+22.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Esmeralda </span>
+          <span class="role"> Operations Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/bd632467-16f3-429a-abdc-a342cf41c708_Debbi-Wong-Accounting-Manager.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Debbi </span>
+          <span class="role"> Accounting Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/2117d343-7156-4e6f-9002-090ea6342e42_Kenny-Novak-Graphic-Design.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Kenny </span>
+          <span class="role"> Graphic Designer </span>
+        </div>
+      </div>  
+       <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/4ecfbc98-ced8-40ae-8082-ea5c65e06ba8_Madison-Bailey-Production-Manager.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Madison </span>
+          <span class="role"> Production Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/e600c588-2fed-4de6-933a-68406fc2d409_Tempest-Orman-Accounting-Asst.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Tempestt </span>
+          <span class="role"> Accounting Assistent </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/d4e66e1a-98af-4df8-bea9-3f1ace0481c0_Jen-Valdivia-Produciton-Asst-.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Jen </span>
+          <span class="role"> Production Assistant </span>
+        </div>
+      </div> 
+      <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/bffdf4de-2a14-43b1-b47b-4ad306efc9d2_Alex-Coleman-Logistics-Manager-.jpg?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Alex </span>
+          <span class="role"> Logistics Manager </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/000de66d-2ba2-4842-9383-305d3ddc72e8_AfterlightImage+20.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Jeff </span>
+          <span class="role"> Graphic Designer </span>
+        </div>
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/55f2fb68-1c1d-45c5-a0a4-3a877915636c_AfterlightImage+21.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Duke </span>
+          <span class="role"> Retail Merchandiser </span>
+        </div>
+      </div>
+       <div class="card-list">
+        <div class="card">
+          <img src="https://images.prismic.io/andersonbrothers/1eb15567-0d64-4d42-a3cb-8bd6d08fdf1f_AfterlightImage+23.JPG?auto=compress,format&w=330&h=370&fit=crop&q=85&f=center" alt="">
+          <span class="name"> Ashley </span>
+          <span class="role"> Retail Merchaniser </span>
+        </div>
+        <div class="card button">
+          <span class="name-1"> WE'RE GROWING </span>
+          <span class="role-1"> JOIN THE FAMILY</span>
+        </div>
+       
+      </div>    
+    </div>
+  </div>
+  <div class="slide-five">
+      <div class="desc-container">
+      <span class="desc-1">REALLY DAMN GOOD STUFF</span>
+      <div class="card-item">
+        <div class="card">
+        <img src="https://images.prismic.io/andersonbrothers/36aef18f-d5e1-4c56-bf08-a10f7de29606_duke.png?auto=compress,format&w=553&h=372&fit=crop&q=85&f=center" alt="">
+        <span class="name">The DUKE</span>
+        <span class="role"> Mascot</span>
+        </div>
+   </div> 
+    </div>
+  </div> 
+</div>
+</template>
+
+<style lang="scss" scoped>
+.body-container{
+  position: relative;
+  width: 100vw;
+  min-height: 100vh;
+  min-height: calc((var(--vh, 1vh) * 100));
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+.slide-one{
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+   img.bg-content{
+      position: absolute;
+      width: 100vw;
+      height: 100vh;
+    }
+    .desc-container{
+        position: relative;
+        display: flex;
+        width: 40%;
+        height: 100%;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        margin-left:  4rem;
+        margin-top: 2rem;
+        line-height: 1.5;
+        span.desc{
+            font-family: 'Antonio';
+            color: #b48645;
+            font-size: 1rem;
+            font-weight: 900;
+        }
+        span.desc-1{
+          color: white;
+          font-family: 'Antonio';
+          font-size: 3rem;
+          font-weight: 900;
+        }
+      .button{
+        position: relative;
+        display: flex;
+        width: 11rem;
+        height: 1.5rem;
+        background: white;
+        justify-content: center;
+        align-items: center;
+        margin-top: 0.5rem;
+        span{
+          color: black;
+          font-family: Antonio;
+          font-size: 0.8rem;
+          font-weight: 900;
+        }
+      }
+    }
+ }
+ .slide-two{
+   position: relative;
+   width: 100vw;
+   height: 100vh;
+   display: flex;
+   flex-direction: column;
+   background-color: #f4f4f4;
+   .img-container{
+      position: absolute;
+      width: 50%;
+      height: 50%;
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+    img.bg-content{
+      position: absolute;
+      margin-top: 2rem;
+      margin-bottom: 2rem;
+      margin-left: 2rem;
+      width: 50vw;
+      height: 100vh;
+    }
+   }
+    .desc-container{
+        position: relative;
+        display: flex;
+        width: 40%;
+        height: 100%;
+        margin-left: 30rem;
+        padding: 5rem 2rem;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        span.desc{
+            font-family: Antonio;
+            color: black;
+            font-size: 20px;
+            font-weight: 900;
+            transform: translateY(2rem);
+          }
+        span.desc-1{
+                font-family: Quicksand;
+                color: black;
+                font-size: 15px;
+                transform: translateY(5rem);
+            }
+          }
+        .img-list{
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          width: 50%;
+          margin-top: 15rem;
+          justify-content: center;
+          align-items: flex-start;
+          margin-left: 1rem;
+          img{
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: flex-start;
+          margin-left: 1rem;
+          }
+        }
+    }
+ 
+.slide-three{
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  .img-container{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+      img.bg-content{
+         position: relative;
+         width: 50vw;
+         height: 100vh;
+         object-fit: cover;
+      }
+  }
+    .desc-container{
+      position: relative;
+      display: flex;
+      width: 40%;
+      height: 100%;
+      margin-left: 30rem;
+      padding: 5rem 2rem;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+    span.desc-1{
+        font-family: Antonio;
+        color: black;
+        font-size: 1rem;
+        font-weight: 900;
+        transform: translateY(-2.5rem);
+    }
+    span.desc-2{
+        font-family: Quicksand;
+        color: black;
+        font-size: 0.7rem;
+        transform: translateY(-1rem);
+    }
+   }
+  }
+.slide-four{
+  position: relative;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+.desc-container{
+  position: relative;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  span.desc-1{
+        font-family: Antonio;
+        color: black;
+        margin: 4rem 0 2rem 0;
+        font-size: 1rem;
+        font-weight: 900;
+    }
+  .card-list{
+    position: relative;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+      .card{
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        img{
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: flex-start;
+          margin-left: 1rem;
+        }
+        span.name{
+          font-family: 'Quicksand';
+          font-size: 18px;
+          margin-top: 0.5rem;
+          margin-bottom: 0.5rem;
+          font-weight: bold;
+          justify-content: flex-start;
+          align-items: flex-start;
+          display: flex;
+          margin-left: 1rem;
+        }
+        span.role{
+          font-family: 'Quicksand';
+          font-size: 15px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          display: flex;
+          margin-left: 1rem;
+          margin-bottom: 1rem;
+        }
+        &.button{
+          background: #f4f4f4;
+          width: 330px;
+          height: 370px;
+          margin-left: 1rem;
+          transform: translateY(-1.89rem);
+          span.name-1{
+            font-family: 'Antonio';
+            font-size: 45px;
+            line-height: 2rem;
+            width: 40%;
+            margin-top: 3rem;
+            margin-bottom: 1rem;
+            font-weight: bold;
+            justify-content: center;
+            text-align: center;
+            align-items: center;
+            display: flex;
+            margin-left: 4rem;
+          }
+          span.role-1{
+            font-family: 'Antonio';
+            font-size: 25px;
+            margin-top: 2rem;
+            margin-bottom: 0.5rem;
+            font-weight: bold;
+            justify-content: flex-start;
+            align-items: flex-start;
+            display: flex;
+            color: #b48645;
+            margin-left: 4rem;
+          }
+          &:hover{
+            background: #b48645;
+            span.role-1{
+              color: #f4f4f4
+            }
+          }
+        }
+      }
+  }
+}
+}
+.slide-five{
+  position: relative;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+.desc-container{
+  position: relative;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  span.desc-1{
+        font-family: Antonio;
+        color: black;
+        width: 40%;
+        text-align: center;
+        margin: 4rem 0 2rem 0;
+        font-size: 4rem;
+        font-weight: 900;
+    }
+}
+.card-item{
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  .card{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    img{
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      margin-left: 1rem;
+      transform: translateY(-3rem);
+    }
+    span.name{
+      font-family: 'Quicksand';
+      font-size: 15px;
+      width: 100%;
+      margin-top: 0.5rem;
+      margin-bottom: 0.5rem;
+      font-weight: bold;
+      justify-content: center;
+      align-items: flex-start;
+      display: flex;
+      margin-left: 1rem;
+      transform: translateY(-6rem);
+    }
+    span.role{
+      font-family: 'Quicksand';
+      font-size: 10px;
+      width: 100%;
+      justify-content: center;
+      align-items: flex-start;
+      display: flex;
+      margin-top: 0.5rem;
+      margin-bottom: 1rem;
+       transform: translateY(-6.8rem);
+    }
+  }
+}
+}
+}
+</style>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="body-container">
     <div class="slide-one">
